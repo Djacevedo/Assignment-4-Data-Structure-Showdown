8c7802d8049388a9b28d4695f45edfdb71f75628
Djacevedo <djacevedo05@gmail.com>
2025-10-17 22:15:31 +0000

    timed challenge

--- timed_challenge.py
+++ timed_challenge.py
@@ -1,3 +1,11 @@
 # Pick one question from timed_challenge.txt
 # Paste the question as a comment below
 # Set a timer for 30 minutes and complete the question!
+def removeDuplicatesKeepOrder(values):
+    seen=set()
+    result=[]
+    for v in values:
+        if v not in seen:
+            result.append(v)
+            seen.add(v)
+            return result
